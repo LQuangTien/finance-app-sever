73d376e5adfafe3a6ba922c01bb4a9e71f8b837a
LQuangTien <tienkg5554@gmail.com>
2020-08-25 12:21:46 +0700

    fix err

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const authMiddleware = require("./middlewares/auth.middleware");
 const app = express();
 app.use(cors());
 app.use(logger("dev"));
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
